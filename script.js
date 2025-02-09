f1b19aa38227279a3cea08910e88ddc8d4f1518d
tommasofacchin <81576746+tommasofacchin@users.noreply.github.com>
2025-02-09 23:04:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -46,18 +46,18 @@ languageToggleBtn.addEventListener("click", () => {
     date3.textContent = "(2022 - current)";
     date4.textContent = "(January 2020)";
     subtitle1.textContent = "IT and telecommunications";
-    subtitle2.textContent = "Bachelor's degree - computer science";
+    subtitle2.textContent = "Bachelor's degree - data science";
     subtitle3.textContent = "Android App Development";
     subtitle4.textContent = "Software Development";
     subtitle5.textContent = "School internship";
     desc1.textContent = "Technical institute with a specialization on IT and communication where I acquired practical and theoretical skills in various fields such as programming, algorithm design, database management, and computer security.";
-    desc2.textContent = "Bachelor's degree in computer science at the university of Venice, where i acquired specialized skills in programming, mathematics, and data analysis.";
+    desc2.textContent = "Bachelor's degree in Data Science from the University of Venice, where I gained specialized skills in programming, mathematics, data analysis, data mining, and neural networks. Key courses included: Calculus, Linear Algebra, Statistics, Data Analysis, Data and Web Mining, and Computational Intelligence.";
     desc3.textContent = "Three week internship where I had the opportunity to develop android applications using the flutter framework. My work included designing and developing customized applications using the Dart programming language, and I gained a several skills such as knowledge of the framework and the ability to create custom layouts and widgets. I also learned how to work in a professional development environment.";
     desc4.textContent = "One-month internship where I learned software and web development,  I also managed documents for digitalization 4.0. Using Windows Form and the C# language I developed a software with a user interface that could manage a web dashboard. I created the structure of this dashboard using HTML, CSS, and JavaScript. In this internship I learned how to work in a professional environment and collaborate with other team members";
     desc5.textContent = "two-week school internship in which i organized personalized tablets and installated custom applications for couriers.";
     contact1.textContent = "CONTACTS";
     contact2.textContent = "CONTACT ME";
-    myDescription.textContent = "I am a software developer with a deep passion for computer science and eager to explore its many fascinating aspects.";
+    myDescription.textContent = "I’m a software developer studying machine learning, eager to explore its fascinating aspects.";
     soft1.textContent = "Visual studio is the main software that i use to write code.";
     soft2.textContent = "environment I used to develop mobile applications.";
     soft3.textContent = "framework I used to develop mobile applications.";
@@ -79,18 +79,18 @@ languageToggleBtn.addEventListener("click", () => {
     date3.textContent = "(2022 - corrente)";
     date4.textContent = "(Gennaio 2020)";
     subtitle1.textContent = "informatica e telecomunicazioni";
-    subtitle2.textContent = "laurea in informatica";
+    subtitle2.textContent = "laurea in data science";
     subtitle3.textContent = "sviluppo applicazioni android";
     subtitle4.textContent = "sviluppo software";
     subtitle5.textContent = "Alternanza Scuola-Lavoro";
     desc1.textContent = "Istituto tecnico superiore con indirizzo informatico in cui ho acquisito competenze pratiche e teoriche in diversi ambiti, come programmazione, progettazione di algoritmi, gestione di database e sicurezza informatica.";
-    desc2.textContent = "Laurea triennale in informatica all'università di venezia, dove ho acquisito competenze specializzate in programmazione, matematica e analisi dei dati";
+    desc2.textContent = "Laurea triennale in data science all'università di venezia, dove ho acquisito competenze specializzate in programmazione, matematica, analisi dei dati e reti neurali";
     desc3.textContent = "Stage di 3 settimane in cui ho avuto l'opportunità di sviluppare applicazioni Android utilizzando il framework Flutter. il mio lavoro ha incluso la progettazione e lo sviluppo di applicazioni personalizzate con linguaggio Dart, acquisendo una vasta gamma di competenze in Flutter, come la conoscenza del framework, la capacità di creare layout e widget personalizzati. Ho inoltre imparato a lavorare in un ambiente di sviluppo professionale.";
     desc4.textContent = "Stage di un mese durante il quale ho sviluppato un software e una dashboard web per l’azienda, ho inoltre gestito documenti per la digitalizzazione 4.0. Il software sviluppato in Windows Form, con linguaggio C#,  aveva la funzionalità di gestire la dashboard attraverso un interfaccia utente, la struttura di questa dashboard l’ho realizzata utilizzando HTML, css e javascript. Ho imparato a lavorare in un ambiente di sviluppo professionale e a collaborare con altri membri del team.";
     desc5.textContent = "Stage scolatico in azienda di 2 settimane in cui ho lavorato nella preparazione di tablet personalizzati e installazione di applicazioni per corrieri.";
     contact1.textContent = "CONTATTI";
     contact2.textContent = "CONTATTAMI";
-    myDescription.textContent = "Sono uno sviluppatore con una forte passione per l'informatica e una grande voglia di esplorare questo mondo.";
+    myDescription.textContent = "Sono uno sviluppatore software e sto studiando machine learning, desideroso di esplorare questo mondo.";
     soft1.textContent = "Visual studio è il software che uso principalmente per scrivere codice.";
     soft2.textContent = "Ambiente che ho utilizzato per sviluppare applicazioni mobile.";
     soft3.textContent = "framework che ho utilizzato per sviluppare applicazioni mobile android.";
@@ -408,7 +408,7 @@ window.addEventListener('load', function() {
         for (var i = 0; i < socialIcons.length; i++) {
           socialIcons[i].classList.add('show');
         }
-      }, 1000);
+      }, 100);
       
     date1.textContent = "(June 2021)";
     date2.textContent = "(July 2023)";
